b0f099e613133cf5a0d634e179c980e974e82ec5
DavidMealha <davidmealha@hotmail.com>
2016-11-18 10:46:24 +0000

    trying to move equipment

--- Prototype/scripts/main.js
+++ Prototype/scripts/main.js
@@ -21,3 +21,51 @@ function closeNavRight() {
     document.getElementById("mySidenav2").style.width = "0";
     document.getElementById("main").style.marginRight = "0";
 }
+
+//to close the menu when the screen is tablet or smaller
+$(window).resize(function() {
+    if ($(window).width() < 768) {
+        $('.sidenav').css({'width':'0'});
+        $('#main').css({'marginLeft':'0'});
+        $('.header img').css('margin-left', '0');
+    }
+    else {
+        $('.sidenav').css({'width':'200px'});
+        $('#main').css({'marginLeft':'200px'});
+        $('.header img').css('margin-left', '170px');
+    }
+});
+
+
+$(document).ready(function(){
+    function fixPageXY(e) {
+      if (e.pageX == null && e.clientX != null ) { 
+        var html = document.documentElement
+        var body = document.body
+
+        e.pageX = e.clientX + (html.scrollLeft || body && body.scrollLeft || 0)
+        e.pageX -= html.clientLeft || 0
+        
+        e.pageY = e.clientY + (html.scrollTop || body && body.scrollTop || 0)
+        e.pageY -= html.clientTop || 0
+      }
+    }
+
+    document.getElementById('lamp').onmousedown = function() {
+      this.style.position = 'absolute'
+
+      var self = this
+
+      document.onmousemove = function(e) {
+        e = e || event
+        fixPageXY(e)  
+        // put ball center under mouse pointer. 25 is half of width/height
+        self.style.left = e.pageX-25+'px' 
+        self.style.top = e.pageY-25+'px' 
+      }
+      this.onmouseup = function() {
+        document.onmousemove = null
+      }
+    }
+
+});
